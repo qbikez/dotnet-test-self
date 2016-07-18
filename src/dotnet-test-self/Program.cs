579ee8e432d996cf472eeb84e25396ba01b60993
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-07-18 13:27:33 +0200

    intial commit

---
+++ src/dotnet-test-self/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace DotnetTestSelf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("args:");
+            foreach (var a in args) {
+                System.Console.WriteLine(a);
+            }
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "dotnet.exe";
+            var largs = args.ToList();
+            largs.Insert(0, "run");
+            proc.StartInfo.Arguments = string.Join(" ", largs);
+            
+            proc.Start();
+            
+        }
+    }
+}
